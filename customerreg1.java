649517082af84eb3de751dfe2363456c5ae710f1
9061711428 <54677210+9061711428@users.noreply.github.com>
2019-11-30 14:32:16 +0530

    Add files via upload

---
+++ customerreg1.java
@@ -0,0 +1,33 @@
+package Calc;
+
+import java.util.Scanner;
+
+public class customerreg1 {
+	  int id;
+		String name;
+		String address;
+		String ph;
+		String email;
+		String ptype;
+		String pid;
+		//public static int i=1;
+		public void register(int id,String n,String a,String p,String e,String pt,String pi)
+		{
+		Scanner ob=new Scanner(System.in);
+		String op1,op2;
+		this.name=n;
+		this.address=a;
+		this.ph=p;
+		this.email=e;
+		this.ptype=pt;
+		this.pid=pi;
+		this.id=id;
+		}//i++;
+		    
+		    void viewid()
+		    {
+		    	System.out.println(id+"\t\t"+name);
+		    }
+		    
+
+}
